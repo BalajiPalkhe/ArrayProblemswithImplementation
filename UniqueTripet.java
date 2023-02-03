00cc2b0df47ac9d9a07a857a8d47d706f90dbcd4
Balaji Palkhe <122020759+BalajiPalkhe@users.noreply.github.com>
2023-02-03 17:53:21 +0530

    Add files via upload

---
+++ UniqueTripet.java
@@ -0,0 +1,43 @@
+package Opps;
+
+public class UniqueTripet 
+{
+
+	
+	   static boolean find3Numbers(int A[], int arr_size, int sum)
+	    {
+	        int l, r;
+	 
+	        // Fix the first element as A[i]
+	        for (int i = 0; i < arr_size - 2; i++) {
+	 
+	            // Fix the second element as A[j]
+	            for (int j = i + 1; j < arr_size - 1; j++) {
+	 
+	                // Now look for the third number
+	                for (int k = j + 1; k < arr_size; k++) {
+	                    if (A[i] + A[j] + A[k] <= sum) {
+	                        System.out.print("Triplet is " + A[i] + ", " + A[j] + ", " + A[k]);
+	                        return true;
+	                    }
+	                }
+	            }
+	        }
+	 
+	        // If we reach here, then no triplet was found
+	        return false;
+	    }
+	 
+	    // Driver program to test above functions
+	    public static void main(String[] args)
+	    {
+	       // FindTriplet triplet = new FindTriplet();
+	        int A[] = { 1, 4, 45, 6, 10, 8 };
+	        int sum = 12;
+	        int arr_size = A.length;
+	 
+	        find3Numbers(A, arr_size, sum);
+	    }
+	}
+
+
